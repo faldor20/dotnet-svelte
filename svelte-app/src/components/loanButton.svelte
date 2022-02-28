89c7a92c3d0f2e4205789480764cfa47c765e534
Eli Dowling <eli.jambu@yahoo.com>
2022-02-28 14:03:48 +1000

    Fixed reseve button being able to be pressed twice Reserving a sedind time wasn't stoped untill the reserve requst had completed.

--- svelte-app/src/components/loanButton.svelte
+++ svelte-app/src/components/loanButton.svelte
@@ -22,6 +22,7 @@
     }
     async function loanBook(userId: number, book: Book) {
         const bookId = book.id;
+        canReserve = false;
         let lend = await api.api.libraryLendBook(userId, bookId);
         if (lend.ok) {
             console.log("loan res:", lend);
@@ -29,12 +30,12 @@
             if (a.ok) {
                 listing.books = a.data;
                 userData?.loanedBooks?.push(book);
-                canReserve = false;
                 console.log("Got new loaninfo");
             } else {
                 console.error("Could not get new Listing Info");
             }
         } else {
+            canReserve=true;
             console.error("lending error: ", lend.statusText);
         }
 
